7d7328b3c17810428086612e6ebfd4959b82c60b
ajdinkecanovic <kecanovicajdin@gmail.com>
2023-02-24 10:35:27 +0100

    Revert "implementing animations"
    This reverts commit 5a826f1848394233088e71819a24ee41235b1bf1.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,11 @@
 import '@/styles/globals.css'
 import Layout from 'components/Layout'
-import TransitionEffect1 from 'components/TransitionEffect1'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
-      <TransitionEffect1>
       <Component {...pageProps} />
-      </TransitionEffect1>
     </Layout>
   )
 }
