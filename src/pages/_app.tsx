5a826f1848394233088e71819a24ee41235b1bf1
ajdinkecanovic <kecanovicajdin@gmail.com>
2023-02-24 10:09:08 +0100

    implementing animations

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import Layout from 'components/Layout'
+import TransitionEffect1 from 'components/TransitionEffect1'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <TransitionEffect1>
       <Component {...pageProps} />
+      </TransitionEffect1>
     </Layout>
   )
 }
