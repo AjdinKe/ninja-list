c122f0afdbe276db9ded2df9f5b4453940576d7c
ajdinkecanovic <kecanovicajdin@gmail.com>
2023-02-24 10:35:59 +0100

    Revert "Revert "added animation""
    This reverts commit 720808764196e4c2f0173608d5c36e2a8ebe432a.

---
+++ components/TransitionEffect1.js
@@ -0,0 +1,44 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
+
+const variants = {
+  in: {
+    opacity: 1,
+    scale: 1,
+    z: 0,
+    transition: {
+      duration: 0.5,
+      delay: 0
+    }
+  },
+  out: {
+    opacity: 0,
+    scale: 1,
+    z: 40,
+    transition: {
+      duration: 0.5
+    }
+  }
+};
+
+const TransitionEffect1 = ({ children }) => {
+  const { asPath } = useRouter();
+
+  return (
+    <div className="effect-1">
+      <AnimatePresence initial={false} exitBeforeEnter>
+        <motion.div
+          key={asPath}
+          variants={variants}
+          animate="in"
+          initial="out"
+          exit="out"
+        >
+          {children}
+        </motion.div>
+      </AnimatePresence>
+    </div>
+  );
+};
+
+export default TransitionEffect1;
